5df1ba3d9d787ee02a447c85e91cc6fabf22c472
DWARAPUREDDY Mohan <122905156+Mohanlv8978@users.noreply.github.com>
2024-03-02 12:05:49 +0530

    Add files via upload

---
+++ program3.java
@@ -0,0 +1,23 @@
+package project_1;
+import java.util.*;
+public class program3 {
+	public static void main(String[] args) {
+		Scanner m= new Scanner(System.in);
+		System.out.println("enter a number:");
+		int n= m.nextInt();	
+		switch(n) 
+	{
+	case 46:
+		System.out.println("case 1 is working ");
+		break;
+	case 16:
+		System.out.println("case 2 is working");
+		break;
+	default:
+			System.out.println("invalid number");
+	}
+		m.close();
+	}
+}
+	
+ 
