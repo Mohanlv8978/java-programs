60ad4cee9e913805171d2c8c58e20800285af133
DWARAPUREDDY Mohan <122905156+Mohanlv8978@users.noreply.github.com>
2024-03-10 12:15:34 +0530

    Add files via upload

---
+++ arrycomaparing.java
@@ -0,0 +1,19 @@
+package project_1;
+
+public class comaparingarry {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4};
+		int b[]= {4,3,2,1};
+		if(a[0] == b[3]) 
+		{
+			System.out.println("equal");
+		}
+		else 
+		{
+			System.out.println("not equal");
+		}
+
+	}
+
+}
