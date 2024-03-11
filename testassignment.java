f57ff4ab170aa8ba642e5708156e53eeb1f0bbac
DWARAPUREDDY Mohan <122905156+Mohanlv8978@users.noreply.github.com>
2024-03-11 16:04:30 +0530

    Add files via upload

---
+++ testassignment.java
@@ -0,0 +1,12 @@
+package project_1;
+import java.util.*;
+public class program8 {
+	public static void main(String[] args) {
+		int a[] = {2,43,66,92,3,5,13,123,432,44,78};
+		for(int i=0;i<a.length;i++) 
+		{
+			if(a[i]>100)
+		System.out.println(a[i]);	
+		}
+	}
+}
